a18d18f934ad9b9ae2bbe6cc0539ff8b5479a4ba
honake <honami.yuya@gmail.com>
2017-01-02 17:27:05 +0900

    Reflect #11 ~ #15

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,4 +2,15 @@ class DemoController < ApplicationController
   layout false
   def index
   end
+
+  def hello
+  end
+
+  def other_hello
+    redirect_to(action: 'index')
+  end
+
+  def google
+    redirect_to('http://google.com')
+  end
 end
